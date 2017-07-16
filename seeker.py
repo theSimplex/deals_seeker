df8562326138e1945ebf547c3eaca1673241c75c
theSimplex <atsimplex@gmail.com>
2017-07-16 12:44:33 -0500

    More keywords #2

--- seeker.py
+++ seeker.py
@@ -11,7 +11,7 @@ from telegrammer import Telegrammer
 class Seeker(Telegrammer):
     
     crap_list = ['coupon', 'contest', 'chance', 'just', 'only', 'off', 'buy', 'spend',
-                 'sweepstakes', 'win', 'pre-order', 'shipped', 'purchase']
+                 'sweepstakes', 'win', 'pre-order', 'shipped', 'purchase', 'deal']
 
     def __init__(self):
         self.sent = []
